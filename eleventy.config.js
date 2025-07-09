b1e15ea30b35af32b223cb66d5123e82eecf4a63
William Kelso <wpkelso+github@posteo.net>
2025-07-08 20:09:49 -0400

    11ty rework

---
+++ eleventy.config.js
@@ -0,0 +1,26 @@
+import pluginWebc from "@11ty/eleventy-plugin-webc";
+import path from "node:path";
+import * as sass from "sass";
+
+export default function(eleventyConfig) {
+    eleventyConfig.setInputDirectory("_src");
+    eleventyConfig.setOutputDirectory("_site");
+    eleventyConfig.setIncludesDirectory(".");
+
+    eleventyConfig.addPassthroughCopy("_src/img");
+    eleventyConfig.addPassthroughCopy("_src/fonts");
+
+	eleventyConfig.addPlugin(pluginWebc, {
+        // Glob to find no-import global components
+		// This path is relative to the project-root!
+		// The default value is shown:
+		components: "_src/components/**/*.webc",
+        layouts: "_src/layouts/**/*.webc",
+    });
+
+    // Watch CSS files for changes
+    eleventyConfig.setBrowserSyncConfig({
+        files: './_site/css/**/*.css'
+    });
+};
+
